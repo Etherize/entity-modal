da7484527b0897c87d47e614eee84ea6b8e2ff4c
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2021-04-06 20:01:59 -0400

    added entry point of widget

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// element on client-site to target to render this widget//
+const plugin = document.getElementById('entity-modal-plugin')
+
+//button to actually trigger the modal and other screens//
+//@DEV:  this is temp for now, will be refactored into something better//
+const pluginBtn = document.createElement('button')
+pluginBtn.innerHTML = `
+Entity Modal Trigger
+`
+
+plugin.appendChild(pluginBtn)
